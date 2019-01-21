90809c6faf26ed4631c470db71ccd88ccfe66ab4
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-01-21 18:45:06 -0500

    Ménage commentaire + description algorithme

--- src/multiplication_lineaire.cpp
+++ src/multiplication_lineaire.cpp
@@ -2,18 +2,12 @@
 
 void multiplicationLineaire(const std::vector<int> &A, std::vector<int> &B)
 {
-    // Insérer votre code ici
-
-    // Notez que la fonction ne retourne pas de vecteur, elle modifie le
-    // vecteur B directement.
     const int IDENTITY_ELEMENT = 1;
     const int ABSORBING_ELEMENT = 0;
     const int VECTOR_SIZE = A.size();
 
     int totalProductExceptZeroes = IDENTITY_ELEMENT;
 
-    //std::unordered_map<int, int> map1 = std::unordered_map<int, int>(); À cause du pire cas en N, on peut arriver en n^2
-    //std::vector<int> absorbingIndicesVector = std::vector<int>(VECTOR_SIZE); //O(n) pourrait être O(1) sans le paramètre, mais forcerait un constant ammorti
     std::vector<int> absorbingIndicesVector = std::vector<int>();
 
     // Parcourir tous les éléments du vecteur en multipliant les nombres non-absorbant et en conservant
